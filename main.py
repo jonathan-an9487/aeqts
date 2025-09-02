3d77b4268658fcac62da9cc5e677fb97580896a0
razeo0251 <jha0.0ananan@gmail.com>
2025-09-02 23:55:02 +0800

    可以跑了數據也對的上，但怪怪的

--- main.py
+++ main.py
@@ -9,58 +9,112 @@ import math ,random,numpy as np
 
 #     return item_weight,item_price,Capacity
 
-def Initalize_Qubit():
-    alpha = beta = 1/math.sqrt(2)
-    qubits=[]
-    for j in range(0,):
-        qubits.append([alpha,beta])
+def weight(solution,item_weight):
+    total = 0
+    for bit,w in zip(solution,item_weight):
+        if bit == 1:
+            total += w 
+    return total
+
+def Repair (solution,item_weight,capacity):
+    sol = solution[:]
+
+    while weight(sol,item_weight)>capacity:
+        ones = [i for i,bit in enumerate(sol) if bit ==1]
+        ratios = [(item_price[i]/item_weight[i],i) for i in ones]
+        _, worst_index = min(ratios,key=lambda x:x[0])
+        sol[worst_index] = 0
+
+    return sol
+
+def measure(qubits):
+    solution = []
+
+    for alpha,beta in qubits:
+        p_0 = alpha**2
+
+        if random.random() <p_0 :
+            solution.append(0)
+        else:
+            solution.append(1)
+    return solution
+def update_qubits(K,N,qubits,population,theta):
+    population.sort(key=lambda x:x[1],reverse=True)
+    for i in range(1,(N//2)+1):
+        best = population[i-1][0]
+        worst = population[-i][0]
+
+        for j in range(K):
+
+            alpha,beta = qubits[j]
+
+            if best[j]==1 and worst[j]==0:  delta = theta/i
+            elif best[j]==0 and worst[j]==1:    delta = theta/i
+            else:   delta=0
+
+            alpha_lamda = alpha*math.cos(delta) - beta*math.sin(delta)
+            beta_lamda = alpha*math.sin(delta) + beta*math.cos(delta)
+
+            norm = math.sqrt(alpha_lamda**2 + beta_lamda**2)
+            qubits[j] = [alpha_lamda/norm,beta_lamda/norm]
+
     return qubits
 
-def Initalize_the_first_population():
+def Initalize_Qubit(K):
+    alpha = beta = 1/math.sqrt(2)
+    qubits=[]
+    for j in range(0,K):
+        qubits.append([alpha,beta])
+
+    return qubits
+
+def Initalize_population(N,qubits,item_weight,item_price,capacity):
     population=[] 
-    for i in range(0,):
-        population = Initalize_Qubit()
-        if Initalize_Qubit > capacity:
-            Repair(population) #重量超過容量 就remove
-        superbeach = evaluate(population)  #計算profit
-    return superbeach
+    for i in range(N):
+        sol = measure(qubits)#測量
+        if  weight(sol,item_weight)> capacity:
+            sol = Repair(sol,item_weight,capacity) 
 
-def Iteraion():#1->MAX_ITER
+        fitness = evaluate(sol,item_price)
+        population.append((sol,fitness))
+
+    return population
+
+def evaluate(solution,item_price):
+    return sum(p for bit,p in zip(solution, item_price) if bit ==1)
+
+def Iteraion(item,p_size,ITER,theta,item_weight,item_price,capacity):
+    qubits = Initalize_Qubit(item)
+    sb= None
     t=0
-    while t <= MAX_ITER:
-        superbeach = Initalize_the_first_population()    #更新Population
-        Initalize_Qubit()   #更新qubits
-        superbeach.sort()
-        for i in range(1,N/2):
-            best = superbeach[i]
-            worst = superbeach[i-(N/2)-1]
-            for j in range (1 ,K):
-                if best[j]==1 and worst[j] == 0:
-                    qubit[j]=qubit[j]-avg_theta
-                if best[j] ==0 and worst[j] ==1:
-                    qubit[j] = qubit[j]+avg_theta
-                else:
-                    pass
+    for t in range(ITER+1):
+        population = Initalize_population(p_size,qubits,item_weight,item_price,capacity)    #更新Population
+        population.sort(key=lambda x:x[1], reverse=True)
+        
+        if sb is None or population[0][1] >sb[1]:
+            sb = population[0]
 
-        best_solution = superbeach
-        if best_solution > superbeach:
-            superbeach = best_solution
+        qubits= update_qubits(item,p_size,qubits,population,theta)
 
-    t=t+1
+        b = population[0]
+
+        if b[1] > sb[1]:
+            sb = b
+
+        print(f"iter{t}:best fitness = {population[0][1]}")
+    return sb
 
 if __name__ == "__main__":
-    K=10 #物品數 
-    N=5 #族群大小(Population size)
-    MAX_ITER=10  #最大迭帶次數 
-    avg_theta=0.01*3.14159 #旋轉角度
+    item=250 #物品數 K
+    p_size=50 #族群大小(Population size)N
+    ITER=200  #迭帶次數 
+    theta=0.01*math.pi #旋轉角度
 
     base_weight = np.array([1,2,3,4,5,6,7,8,9,10])
-    num_repeat = (K//10)+1
-    item_weight = np.tile(base_weight,num_repeat)[:K]
+    num_repeat = (item//10)+1
+    item_weight = np.tile(base_weight,num_repeat)[:item]
 
     item_price =item_weight+5
     capacity=0.5*np.sum(item_weight)
 
-    Iteraion()
-
-
+    Iteraion(item,p_size,ITER,theta,item_weight,item_price,capacity)
